944e9fc30d9e53b2ce878da1b35d6a1ab4c80ae7
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-04-25 13:31:34 +0900

    fix hacker

--- src/index.ts
+++ src/index.ts
@@ -3,49 +3,33 @@ import { Level } from "bdsx/bds/level";
 import { CompoundTag } from "bdsx/bds/nbt";
 import { TextPacket } from "bdsx/bds/packets";
 import { Player } from "bdsx/bds/player";
-import { serverInstance } from "bdsx/bds/server";
 import { pdb } from "bdsx/core";
-import { UNDNAME_NAME_ONLY } from "bdsx/dbghelp";
 import { events } from "bdsx/event";
+import { bedrockServer } from "bdsx/launcher";
 import { NativeClass } from "bdsx/nativeclass";
 import { int32_t, void_t } from "bdsx/nativetype";
-import { ProcHacker } from "bdsx/prochacker";
+import { procHacker } from "bdsx/prochacker";
+import * as fs from "fs";
+import * as path from "path";
 import { MapItemSavedData } from "./map-data";
 
-const path = require("path");
-const fs = require("fs");
-
-export const IMAGE_PATH = path.join(process.cwd(), '../images');
+export const IMAGE_PATH = path.join(process.cwd(), "../images");
 
 export class LevelStorage extends NativeClass {}
 
 if (!fs.existsSync(IMAGE_PATH)) fs.mkdirSync(IMAGE_PATH);
-
-const procHacker = ProcHacker.load(path.join(__dirname, '../pdb.ini'), [
-    "Level::getLevelStorage",
-    "MapItemSavedData::setPixel",
-    "MapItemSavedData::setLocked",
-    "MapItemSavedData::getParentMapId",
-    "MapItemSavedData::save"
-], UNDNAME_NAME_ONLY);
-
 pdb.setOptions(0);
-const procHackerDec = ProcHacker.load(path.join(__dirname, '../pdb.ini'), [
-    "?getMapSavedData@Level@@UEAAPEAVMapItemSavedData@@AEBVCompoundTag@@@Z"
-]);
-pdb.close();
 
-MapItemSavedData.prototype.save = procHacker.js("MapItemSavedData::save", void_t, {this: MapItemSavedData}, LevelStorage);
-MapItemSavedData.prototype.setPixel = procHacker.js("MapItemSavedData::setPixel", void_t, {this: MapItemSavedData}, int32_t, int32_t, int32_t);
-MapItemSavedData.prototype.setLocked = procHacker.js("MapItemSavedData::setLocked", void_t, {this: MapItemSavedData});
-MapItemSavedData.prototype.getMapId = procHacker.js("MapItemSavedData::getParentMapId", ActorUniqueID, {this: MapItemSavedData});
+MapItemSavedData.prototype.save = procHacker.js("?save@MapItemSavedData@@QEAAXAEAVLevelStorage@@@Z", void_t, { this: MapItemSavedData }, LevelStorage);
+MapItemSavedData.prototype.setPixel = procHacker.js("?setPixel@MapItemSavedData@@QEAAXIII@Z", void_t, { this: MapItemSavedData }, int32_t, int32_t, int32_t);
+MapItemSavedData.prototype.setLocked = procHacker.js("?setLocked@MapItemSavedData@@QEAAXXZ", void_t, { this: MapItemSavedData });
+MapItemSavedData.prototype.getMapId = procHacker.js("?getParentMapId@MapItemSavedData@@QEBA?AUActorUniqueID@@XZ", ActorUniqueID, { this: MapItemSavedData });
 
-export const _getLevelStorage = procHacker.js("Level::getLevelStorage", LevelStorage, null, Level);
-export const getMapData = procHackerDec.js("?getMapSavedData@Level@@UEAAPEAVMapItemSavedData@@AEBVCompoundTag@@@Z", MapItemSavedData, null, Level, CompoundTag);
-
+export const _getLevelStorage = procHacker.js("?getLevelStorage@Level@@UEBAAEBVLevelStorage@@XZ", LevelStorage, null, Level);
+export const getMapData = procHacker.js("?getMapSavedData@Level@@UEAAPEAVMapItemSavedData@@AEBVCompoundTag@@@Z", MapItemSavedData, null, Level, CompoundTag);
 
 export function getLevel(): Level {
-    return serverInstance.minecraft.getLevel();
+    return bedrockServer.level;
 }
 
 export function getLevelStorage(): LevelStorage {
@@ -61,5 +45,5 @@ export function sendMessage(player: Player, message: string, type: TextPacket.Ty
 }
 
 events.serverOpen.on(() => {
-    require('./command');
+    require("./command");
 });
